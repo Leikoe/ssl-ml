a10314b13dfbeec9491b5253cd09c5c523ce2c35
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-01-26 13:40:41 +0100

    added deepmind's soccer

--- mujoco/dm_helpmepls.py
+++ mujoco/dm_helpmepls.py
@@ -5,7 +5,7 @@ import numpy as np
 from dm_control.locomotion import soccer as dm_soccer
 from dm_env import TimeStep
 
-env = dm_soccer.load(team_size=2,
+env = dm_soccer.load(team_size=5,
                      time_limit=10.0,
                      disable_walker_contacts=False,
                      enable_field_box=True,
@@ -17,7 +17,7 @@ action_specs = env.action_spec()
 # Define a uniform random policy.
 def random_policy(time_step: TimeStep):
     # del time_step  # Unused.
-    print(time_step.observation)
+    # print(time_step.observation)
     return [np.random.uniform(low=action_spec.minimum,
                               high=action_spec.maximum,
                               size=action_spec.shape) for action_spec in action_specs]
